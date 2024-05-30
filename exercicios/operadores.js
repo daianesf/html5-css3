ec371f825d1b67e6e2be34fd2b1f1e15454353df
daianesf <156373089+daianesf@users.noreply.github.com>
2024-05-30 18:18:25 -0300

    ex. 026

---
+++ exercicios/operadores.js
@@ -0,0 +1,36 @@
+let Multi = 60 * 5
+
+console.log('Multi: ', Multi);
+
+
+let Div = 60 / 5
+
+console.log('Div: ', Div);
+
+
+let Sub = 60 - 20
+
+console.log('Div: ', Sub);
+
+// address --> salvo na memoria RAM do sistema: [X001287262]
+let address = "Rua fermil do couto, 51"
+
+
+// [X001287262] <--- "avenida teotonio vilela"
+address = "avenida teotonio vilela"
+
+// [X001287262]
+
+console.log("Endereco: ", address)
+
+
+
+
+const listaNomes = ["pedro", "daiane", "samuel", "daniel"]
+
+listaNomes.pop()
+listaNomes.pop()
+listaNomes.unshift("daniel")
+listaNomes.push("samuel")
+
+console.log(listaNomes)
